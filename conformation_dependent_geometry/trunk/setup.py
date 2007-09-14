baccd98926097ff1e0267289f65f6ea3a5f54d39
Donnie Berkholz <dberkholz@dberkholz.com>
2007-09-14 22:34:31 +0000

    Include ChangeLog by subclassing distutils sdist to run git log.

--- conformation_dependent_geometry/trunk/setup.py
+++ conformation_dependent_geometry/trunk/setup.py
@@ -1,6 +1,16 @@
-from distutils.core import setup
 name='pgd-utils'
 version='0.2.1'
+
+import os
+from distutils.core import setup
+from distutils.command.sdist import sdist as _sdist
+
+class sdist(_sdist):
+    """Specialized source distribution creation"""
+
+    cmd="git log > ChangeLog"
+    os.system(cmd)
+
 setup(name=name,
       version=version,
       description='Package for accessing Protein Geometry Database info',
@@ -12,5 +22,6 @@ setup(name=name,
       data_files=[('share/doc/' + name + '-' + version,
                    ['karplus-definitions.jpg',
                     'examples/pgd-lookup.py',
-                    'examples/test.res'])]
+                    'examples/test.res'])],
+      cmdclass={'sdist': sdist},
       )
