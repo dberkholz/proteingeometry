115d42115046ed5aa84b564992706b273505d739
Donnie Berkholz <dberkholz@dberkholz.com>
2007-09-19 21:40:53 +0000

    get_geom_rmsd.py: Add optparse support.

--- conformation_dependent_geometry/trunk/pgd/get_geom_rmsd.py
+++ conformation_dependent_geometry/trunk/pgd/get_geom_rmsd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import math, sys
+import math, sys, optparse
 import pgd.angles as a
 
 try:
@@ -28,6 +28,11 @@ except:
 #
 # calc_torsion_omega(self)
 # Calculates the Omega torsion angle of the amino acid.
+
+# Program version                                                              
+version = '0.1'
+
+usage = """usage: %prog [options] [<geometry type> <PDB> <PDB>]"""
 
 class geom(object):
     def __init__(self, attr):
@@ -83,27 +88,38 @@ class protein_geometry_database(geom):
 
 
 def main(argv):
-    if len(argv[1:]) != 3:
-        print "Requires a length/angle and 2 PDB files as an argument"
-        sys.exit(1)
+    global optlist
+    global args
+    optlist, args = parse_options()
 
-    meas = argv[1]
-    pdb1 = argv[2]
-    pdb2 = argv[3]
+    # Needed for angles code
+    angles_parser = a.optparse_setup()
+    # Options to use for angles
+    angles_options = []
+    a.optlist, a.args = angles_parser.parse_args(angles_options)
+
+    if len(args) != 3:
+        parser.error('incorrect number of arguments')
+
+    meas = args[0]
+    pdb1 = args[1]
+    pdb2 = args[2]
     struct1 = mmLib.FileIO.LoadStructure(file=pdb1)
     struct2 = mmLib.FileIO.LoadStructure(file=pdb2)
     get_geometry(struct1)
     get_geometry(struct2)
 
-    eh = geom(meas)
-    pgd = protein_geometry_database(meas)
-    #print eh.__dict__
-    if meas == 'ome':
-        #print "Measurement is ome"
-        eh.set(meas, 180)
+    if optlist.compare_eh:
+        eh = geom(meas)
+        # print eh.__dict__
+        if meas == 'ome':
+            # print "Measurement is ome"
+            eh.set(meas, 180)
+        eh.msd = 0
+    if optlist.compare_pgd:
+        pgd = protein_geometry_database(meas)
+        pgd.msd = 0
     msd = 0
-    eh.msd = 0
-    pgd.msd = 0
     N = 0
     for r in struct1.iter_amino_acids():
         r_atom = r.get_atom('C')
@@ -114,46 +130,52 @@ def main(argv):
         if r2_atom.occupancy < 0.5 or r_atom.occupancy < 0.5:
             continue
 
-        # We use this to look up conformation-dependent geometry
-        pgd.res_name = r2.res_name
-        # Look for residue i+1 for a proline, so we know if we're XPro
-        pgd.next_res_numstr = str(int(r2.fragment_id) + 1)
-        r2_dict = r2.get_chain().fragment_dict
-        try:
-            pgd.next_res_name = r2_dict[pgd.next_res_numstr]
-        except:
-            pgd.next_res_name = 'END'
+        if optlist.compare_pgd:
+            # We use this to look up conformation-dependent geometry
+            pgd.res_name = r2.res_name
+            # Look for residue i+1 for a proline, so we know if we're XPro
+            pgd.next_res_numstr = str(int(r2.fragment_id) + 1)
+            r2_dict = r2.get_chain().fragment_dict
+            try:
+                pgd.next_res_name = r2_dict[pgd.next_res_numstr]
+            except:
+                pgd.next_res_name = 'END'
 
-        if meas == 'a3':
-            #print "Measurement is a3"
-            if r2.res_name == 'GLY':
-                #print r2
-                eh.set(meas, 112.5)
-            elif r2.res_name == 'PRO':
-                #print r2
-                eh.set(meas, 111.8)
-            else:
-                eh.set(meas, 111.2)
-        elif meas == 'phi' or meas == 'psi' or meas == 'ome':
+        if optlist.compare_eh:
+            if meas == 'a3':
+                # print "Measurement is a3"
+                if r2.res_name == 'GLY':
+                    # print r2
+                    eh.set(meas, 112.5)
+                elif r2.res_name == 'PRO':
+                    # print r2
+                    eh.set(meas, 111.8)
+                else:
+                    eh.set(meas, 111.2)
+        if meas == 'phi' or meas == 'psi' or meas == 'ome':
             #print "phi/psi/ome"
             if r.props[meas] > 180 or r2.props[meas] > 180:
                 continue
 
         dev = r.props[meas] - r2.props[meas]
         msd += dev**2
-        eh.dev = r2.props[meas] - eh.get(meas)
-        eh.msd += eh.dev**2
-        pgd_meas = pgd.get(r.props['phi'], r.props['psi'], meas)
-        pgd.dev = r2.props[meas] - pgd_meas
-        pgd.msd += pgd.dev**2
+        if optlist.compare_eh:
+            eh.dev = r2.props[meas] - eh.get(meas)
+            eh.msd += eh.dev**2
+        if optlist.compare_pgd:
+            pgd_meas = pgd.get(r.props['phi'], r.props['psi'], meas)
+            pgd.dev = r2.props[meas] - pgd_meas
+            pgd.msd += pgd.dev**2
         N += 1
         #print '%.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %d' % (r.props[meas], r2.props[meas], eh.get(meas), pgd_meas, dev, msd, eh.dev, eh.msd, pgd.dev, pgd.msd, N)
     rmsd = math.sqrt ( msd / N )
-    eh.rmsd = math.sqrt ( eh.msd / N )
-    pgd.rmsd = math.sqrt ( pgd.msd / N )
     print '%s for %s vs %s = %.2f' % (meas, pdb1, pdb2, rmsd)
-    print '%s for %s vs E&H = %.2f' % (meas, pdb2, eh.rmsd)
-    print '%s for %s vs PGD = %.2f' % (meas, pdb2, pgd.rmsd)
+    if optlist.compare_eh:
+        eh.rmsd = math.sqrt ( eh.msd / N )
+        print '%s for %s vs E&H = %.2f' % (meas, pdb2, eh.rmsd)
+    if optlist.compare_pgd:
+        pgd.rmsd = math.sqrt ( pgd.msd / N )
+        print '%s for %s vs PGD = %.2f' % (meas, pdb2, pgd.rmsd)
 
 def get_geometry(struct):
     for r in struct.iter_amino_acids():
@@ -228,6 +250,29 @@ def get_geometry(struct):
             'a6': math.degrees(r.a6),
             'a7': math.degrees(r.a7),
                     }
+
+def parse_options():
+    parser = optparse.OptionParser(version='%prog ' + version)
+    parser.disable_interspersed_args()
+    parser.set_usage(usage)
+    parser.set_defaults(verbose=False, compare_eh=False, compare_pgd=False)
+    parser.add_option( \
+        '-v', '--verbose', \
+            action='store_true', \
+            dest='verbose', \
+            help='Use verbose output; defaults to %default')
+    parser.add_option( \
+        '-e', '--engh-and-huber', \
+            action='store_true', \
+            dest='compare_eh', \
+            help='Compare geometry to Engh & Huber; defaults to %default')
+    parser.add_option( \
+        '-p', '--protein-geometry-database', \
+            action='store_true', \
+            dest='compare_pgd', \
+            help='Compare geometry to Protein Geometry Database; defaults to %default')
+    optlist, args = parser.parse_args()
+    return optlist, args
 
 if __name__ == '__main__':
 	sys.exit(main(sys.argv))
