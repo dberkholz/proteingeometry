505145fb951c689fde983c94bdc60d450a5d580c
Donnie Berkholz <dberkholz@dberkholz.com>
2007-09-20 21:56:20 +0000

    Optimize get_geometry() by only calculating geometry groups if needed.
    Calculate angles, lengths, and torsion angles in separate groups, by
    passing in required angles and only calculating those groups. Cuts
    runtime with -p -e -s file.pdb from 5 seconds to 3.5 seconds.

--- conformation_dependent_geometry/trunk/pgd/get_geom_rmsd.py
+++ conformation_dependent_geometry/trunk/pgd/get_geom_rmsd.py
@@ -107,12 +107,12 @@ def main(argv):
 
     pdb = args[1]
     struct = mmLib.FileIO.LoadStructure(file=pdb)
-    get_geometry(struct)
+    get_geometry(struct, meas)
 
     if optlist.compare_pdb:
         cpdb = optlist.compare_pdb
         cstruct = mmLib.FileIO.LoadStructure(file=cpdb)
-        get_geometry(cstruct)
+        get_geometry(cstruct, meas)
         msd = 0
 
     if optlist.compare_eh:
@@ -222,80 +222,111 @@ def main(argv):
         pgd.rmsd = math.sqrt ( pgd.msd / N )
         print '%s for %s vs PGD = %.2f' % (meas, pdb, pgd.rmsd)
 
-def get_geometry(struct):
+def get_geometry(struct, *geomtypes):
+    # calculate bond angles
+    angle=False
+    # calculate bond lengths
+    length=False
+    # calculate torsions besides phi and psi
+    torsion=False
+    for geomtype in geomtypes:
+        if geomtype.startswith('a'):
+            angle=True
+        elif geomtype.startswith('l'):
+            length=True
+        elif geomtype == 'ome' \
+                or geomtype == 'zeta':
+            torsion=True
+
     for r in struct.iter_amino_acids():
         next_res = r.get_offset_residue(1)
         prev_res = r.get_offset_residue(-1)
 
-        r.a3, r.a2, r.a4, r.a5, a6, a1 = \
-              r.calc_mainchain_bond_angle()
-        r.l2, r.l4, r.l5, r.l3, r.l6 = \
-              r.calc_mainchain_bond_length()
-        r.l7 = 0
-        if prev_res:
-            r.l1 = prev_res.l6
-        else:
-            # n-terminus
-            r.l1 = 0
-            r.a1 = 0
-            r.a6 = 0
-            r.a7 = 0
-        if next_res:
-            next_res.a1 = a1
-            next_res.a6 = a6
-            aN = r.get_atom('N')
-            aCA = r.get_atom('CA')
-            aO = r.get_atom('O')
-            aC = r.get_atom('C')
-            naN = next_res.get_atom('N')
-
-            next_res.l2 = mmLib.AtomMath.calc_distance(aN, aCA)
-            r.l7 = next_res.l2
-            next_res.a7 = mmLib.AtomMath.calc_angle(aO, aC, naN)
-        else:
-            # c-terminus
+        if angle:
+            r.a3, r.a2, r.a4, r.a5, a6, a1 = \
+                r.calc_mainchain_bond_angle()
+        if length:
+            r.l2, r.l4, r.l5, r.l3, r.l6 = \
+                r.calc_mainchain_bond_length()
             r.l7 = 0
+            if prev_res:
+                r.l1 = prev_res.l6
+            else:
+                # n-terminus
+                r.l1 = 0
+                r.a1 = 0
+                r.a6 = 0
+                r.a7 = 0
+        if next_res:
+            if angle:
+                next_res.a1 = a1
+                next_res.a6 = a6
+                aO = r.get_atom('O')
+                aC = r.get_atom('C')
+                naN = next_res.get_atom('N')
+                next_res.a7 = mmLib.AtomMath.calc_angle(aO, aC, naN)
+            if length:
+                aN = r.get_atom('N')
+                aCA = r.get_atom('CA')
+                next_res.l2 = mmLib.AtomMath.calc_distance(aN, aCA)
+                r.l7 = next_res.l2
+
+        else:
+            if length:
+                # c-terminus
+                r.l7 = 0
         r.phi = r.calc_torsion_phi()
         r.psi = r.calc_torsion_psi()
-        r.ome = r.calc_torsion_omega()
+        if torsion:
+            r.ome = r.calc_torsion_omega()
 
-        if not r.l3:
-            # glycine
-            r.l3 = 0
-            r.a2 = 0
-            r.a4 = 0
-        if not r.l6:
-            # c-terminus
-            r.l6 = 0
+        # glycine
+        if length:
+            if not r.l3:
+                r.l3 = 0
+        if angle:
+            if not r.a2:
+                r.a2 = 0
+                r.a4 = 0
+
+        # c-terminus
+        if length:
+            if not r.l6:
+                r.l6 = 0
+
         if not r.phi:
             r.phi = math.radians(999)
         if not r.psi:
             r.psi = math.radians(999)
-        if not r.ome:
-            r.ome = math.radians(999)
+        if torsion:
+            if not r.ome:
+                r.ome = math.radians(999)
 
         r.props = {
             'name': r.res_name,
             'id': r.fragment_id,
             'phi': math.degrees(r.phi),
             'psi': math.degrees(r.psi),
-            'ome': math.degrees(r.ome),
-            'l1': r.l1,
-            'l2': r.l2,
-            'l3': r.l3,
-            'l4': r.l4,
-            'l5': r.l5,
-            'l6': r.l6,
-            'l7': r.l7,
-            'a1': math.degrees(r.a1),
-            'a2': math.degrees(r.a2),
-            'a3': math.degrees(r.a3),
-            'a4': math.degrees(r.a4),
-            'a5': math.degrees(r.a5),
-            'a6': math.degrees(r.a6),
-            'a7': math.degrees(r.a7),
                     }
-
+        if angle:
+            r.props['a1'] = math.degrees(r.a1)
+            r.props['a2'] = math.degrees(r.a2)
+            r.props['a3'] = math.degrees(r.a3)
+            r.props['a4'] = math.degrees(r.a4)
+            r.props['a5'] = math.degrees(r.a5)
+            r.props['a6'] = math.degrees(r.a6)
+            r.props['a7'] = math.degrees(r.a7)
+        if length:
+            r.props['l1'] = r.l1
+            r.props['l2'] = r.l2
+            r.props['l3'] = r.l3
+            r.props['l4'] = r.l4
+            r.props['l5'] = r.l5
+            r.props['l6'] = r.l6
+            r.props['l7'] = r.l7
+        if torsion:
+            r.props['ome'] = math.degrees(r.ome)
+ 
 def optparse_setup():
     usage = """usage: %prog [options] [<geometry type> <PDB> <PDB>]"""
 
