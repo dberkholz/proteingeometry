3038f3f6b4e3d97f86f14ee5242ca5392a4e7baf
Donnie Berkholz <dberkholz@dberkholz.com>
2010-05-04 17:06:29 +0000

    Fix copyright years.

--- conformation_dependent_geometry/trunk/conformation_dependent_geometry/convert_kernel_regressions.py
+++ conformation_dependent_geometry/trunk/conformation_dependent_geometry/convert_kernel_regressions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 
-# Copyright © 2010 Mayo Clinic College of Medicine
+# Copyright © 2009-2010 Mayo Clinic College of Medicine
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
